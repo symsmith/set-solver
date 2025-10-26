1466d234a1ed259d6a5a284a7661c4f4545c90f7
Syméon Smith <symeon.smith@gmail.com>
2025-10-26 16:39:53 +0100

    use turso for prod db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,11 @@ if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 export default defineConfig({
 	schema: './src/lib/server/database/schema.ts',
 	out: './src/lib/server/database/migrations',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dialect: 'turso',
+	dbCredentials: {
+		url: process.env.DATABASE_URL!,
+		authToken: process.env.DATABASE_TOKEN!
+	},
 	verbose: true,
 	strict: true,
 	casing: 'snake_case'
--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,10 +1,15 @@
+import { env } from '$env/dynamic/private';
 import { DATABASE_URL } from '$env/static/private';
 import * as schema from '$lib/server/database/schema';
-import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/libsql';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = new Database(DATABASE_URL);
-
-export const db = drizzle(client, { schema, casing: 'snake_case' });
+export const db = drizzle({
+	connection: {
+		url: DATABASE_URL!,
+		authToken: env.DATABASE_TOKEN!
+	},
+	schema,
+	casing: 'snake_case'
+});
